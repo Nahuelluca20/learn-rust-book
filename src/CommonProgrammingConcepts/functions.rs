c6329ca569ccda40dd36d6e90569966a2954312b
Nahuel Luca <nahuelluca20@gmail.com>
2023-09-15 09:22:29 -0300

    fuctions

---
+++ src/CommonProgrammingConcepts/functions.rs
@@ -0,0 +1,23 @@
+fn main() {
+    print_labeled_measurement(5, 'h');
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {y}");
+
+    let x = five();
+
+    println!("The value of x is: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}
+
+// Functions with Return Values
+
+fn five() -> i32 {
+    5
+}
